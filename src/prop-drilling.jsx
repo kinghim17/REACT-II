087edb442137ed37f70fcbd40850bd8e70b1d910
Himanshu Mohapatra <153461597+kinghim17@users.noreply.github.com>
2025-02-03 09:54:08 +0530

    Update prop-drilling.jsx

--- src/prop-drilling.jsx
+++ src/prop-drilling.jsx
@@ -1,34 +1,34 @@
-// // normal counter 
-// import React, { useState } from "react";
+// normal counter 
+import React, { useState } from "react";
 
-// function App() {
-//   const [count, setCount] = useState(0);
+function App() {
+  const [count, setCount] = useState(0);
 
-//   return (
-//     <div>
-//       <h1>Counter Application</h1>
-//       <DisplayCount count={count} />
-//       <CounterButtons count={count} setCount={setCount} />
-//     </div>
-//   );
-// }
+  return (
+    <div>
+      <h1>Counter Application</h1>
+      <DisplayCount count={count} />
+      <CounterButtons count={count} setCount={setCount} />
+    </div>
+  );
+}
 
-// function DisplayCount({ count }) {
-//   return (
-//     <div>
-//       <h2>Current Count: {count}</h2>
-//     </div>
-//   );
-// }
+function DisplayCount({ count }) {
+  return (
+    <div>
+      <h2>Current Count: {count}</h2>
+    </div>
+  );
+}
 
-// function CounterButtons({ setCount }) {
-//   return (
-//     <div>
-//       <button onClick={() => setCount((prevCount) => prevCount + 1)}>Increase</button>
-//       <button onClick={() => setCount((prevCount) => prevCount - 1)}>Decrease</button>
-//     </div>
-//   );
-// }
+function CounterButtons({ setCount }) {
+  return (
+    <div>
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>Increase</button>
+      <button onClick={() => setCount((prevCount) => prevCount - 1)}>Decrease</button>
+    </div>
+  );
+}
 
 // export default App;
 
